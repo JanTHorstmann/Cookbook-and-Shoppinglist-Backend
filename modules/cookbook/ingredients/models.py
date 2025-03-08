6660370ca420f77c9bd13a7d81a9b7b0ea08d9ed
JanTHorstmann <janhorstmann@yahoo.de>
2025-03-08 20:09:36 +0100

    add a model to ingredients

--- modules/cookbook/ingredients/models.py
+++ modules/cookbook/ingredients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name.capitalize()

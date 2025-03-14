cf5c87e02e6b9d7ad70d56db5a9c98b42cf6d7d8
JanTHorstmann <janhorstmann@yahoo.de>
2025-03-14 03:52:10 +0100

    add Viewset for Recipe and changed RecipeSerializer and RecipeIngredientSerializer

--- modules/cookbook/recipe/serializers.py
+++ modules/cookbook/recipe/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from .models import Recipe, RecipeIngredient
+from modules.cookbook.ingredients.models import Ingredient 
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
-    ingredient = serializers.StringRelatedField()  # Gibt den Namen der Zutat aus
+    ingredient = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all())  
+    ingredient_name = serializers.CharField(source="ingredient.name", read_only=True)
 
     class Meta:
         model = RecipeIngredient
-        fields = ["ingredient", "amount"]
+        fields = ["ingredient", "ingredient_name", "amount"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["ingredient_name"] = instance.ingredient.name.capitalize()  # Name mit Großbuchstaben
+        return representation
+    
+
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = RecipeIngredientSerializer(source="recipeingredient_set", many=True)
     author = serializers.StringRelatedField()
--- modules/cookbook/recipe/views.py
+++ modules/cookbook/recipe/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Recipe
+from .serializers import RecipeSerializer
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
-# Create your views here.
+    def perform_create(self, serializer):
+        name = serializer.validated_data["name"].strip().lower()
+        serializer.save(name=name)

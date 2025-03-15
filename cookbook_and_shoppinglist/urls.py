016775785b0840d92392feb098a2abe503a8b5e1
JanTHorstmann <janhorstmann@yahoo.de>
2025-03-15 15:06:56 +0100

    add recipe url to main urls

--- cookbook_and_shoppinglist/urls.py
+++ cookbook_and_shoppinglist/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('modules.cookbook.ingredients.urls')),
+    path('api/', include('modules.cookbook.recipe.urls')),
 ]

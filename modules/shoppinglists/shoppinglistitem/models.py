20b9e5d27546e07d17fc89b4f38c5acc8a7cabb0
JanTHorstmann <janhorstmann@yahoo.de>
2025-04-02 04:47:34 +0200

    shoppinglistitem model adjusted again and the serializer created

--- modules/shoppinglists/shoppinglistitem/models.py
+++ modules/shoppinglists/shoppinglistitem/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from modules.cookbook.ingredients.models import Ingredient 
 from django.contrib.auth import get_user_model
 
-class ShoppingList(models.Model):
+class ShoppingListItem(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient_recipes")
     amount = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     unit = models.CharField(max_length=50, blank=False)
@@ -11,9 +11,5 @@ class ShoppingList(models.Model):
     class Meta:
         unique_together = ("ingredient", "author")
 
-    def save(self, *args, **kwargs):
-        self.ingredient = self.ingredient.strip().lower()
-        super().save(*args, **kwargs)
-
     def __str__(self):
-        return self.ingredient.capitalize()
+        return f"{self.amount} {self.unit} {self.ingredient.name.capitalize()}"
---
+++ modules/shoppinglists/shoppinglistitem/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import ShoppingListItem
+from modules.cookbook.ingredients.models import Ingredient
+
+class ShoppingListItemSerializer(serializers.ModelSerializer):
+    ingredient = serializers.CharField()
+
+    class Meta:
+        fields = ["ingredient", "amount", "unit"]
+
+    def create(self, validated_data):
+        ingredient_name = validated_data["ingredient"].strip().lower()
+        shopping_amount = validated_data["amount"]
+        unit = validated_data["unit"]
+        author = validated_data["author"]
+
+        # Ingredient abrufen oder erstellen
+        ingredient, _ = Ingredient.objects.get_or_create(name=ingredient_name)
+
+        # ShoppingListItem abrufen oder erstellen
+        shopping_item, created = ShoppingListItem.objects.get_or_create(
+            ingredient=ingredient,
+            author=author,
+            defaults={"amount": shopping_amount, "unit": unit},
+        )
+
+        if not created:
+            # Falls das Item existiert, Menge aktualisieren
+            shopping_item.amount += shopping_amount
+            shopping_item.save()
+
+        return shopping_item
+            

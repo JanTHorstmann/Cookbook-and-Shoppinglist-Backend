47b2355ed7028c378f0ebfe717c5d71f684fd3d8
JanTHorstmann <janhorstmann@yahoo.de>
2025-04-02 04:57:48 +0200

    create shoppinglistitem viewset url and admin

--- modules/shoppinglists/shoppinglistitem/views.py
+++ modules/shoppinglists/shoppinglistitem/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import ShoppingListItem
+from .serializers import ShoppingListItemSerializer
 
-# Create your views here.
+class ShoppingListItemViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingListItem.objects.all()
+    serializer_class = ShoppingListItemSerializer
+    permission_classes = [IsAuthenticated]  # Nur authentifizierte User dürfen darauf zugreifen
+
+    def get_queryset(self):
+        return ShoppingListItem.objects.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)

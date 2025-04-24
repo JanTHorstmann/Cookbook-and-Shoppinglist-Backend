5e99ae7ce514399544f5c368cff5303e99ed8ae9
JanTHorstmann <janhorstmann@yahoo.de>
2025-04-24 04:13:38 +0200

    create admin inbterface for custom user

--- modules/custom_user/admin.py
+++ modules/custom_user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+
+    # Nur email als Benutzername
+    list_display = ('email', 'is_staff', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )

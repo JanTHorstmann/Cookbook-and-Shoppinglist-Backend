2dc79d0f8882adf4122fa9fd4d1a3df56afeae3b
JanTHorstmann <kontakt@jan-horstmann.eu>
2025-05-12 04:48:37 +0200

    create ResetPasswordIfLoggedIn url

--- auth/reset_password/urls.py
+++ auth/reset_password/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ResetPasswordView, SendResetPasswordMailView
+from .views import ResetPasswordView, SendResetPasswordMailView, ResetPasswordIfLoggedInView
 
 urlpatterns = [
-    path("resetpasswords/<uidb64>/<token>/", ResetPasswordView.as_view(), name="resetpasswordsbruteforce"),
+    path("resetpasswords/<uidb64>/<token>/", ResetPasswordView.as_view(), name="resetpasswords"),
     path("sendresetpasswordmail/", SendResetPasswordMailView.as_view(), name="sendresetpasswordmail"),
+    path("resetpasswordmailifloggedin/", ResetPasswordIfLoggedInView.as_view(), name="resetpasswordmailifloggedin"),
 ]
